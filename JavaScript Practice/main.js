6b274b2e713f72bdd38c8f8fbad8e608b398ffc8
Dakrfox <business.nimh@outlook.com>
2023-03-19 15:48:46 -0500

    fusion navbar & mobile menu

--- JavaScript Practice/main.js
+++ JavaScript Practice/main.js
@@ -1,5 +1,8 @@
 const navMenu = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu')
+const desktopMenu = document.querySelector('.desktop-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const hamIconMenu =  document.querySelector('.menu');
+
 function toggleDesktopMenu(){
     /*(desktopMenu.classList.contains('inactive'))
     ?desktopMenu.classList.remove('inactive')
@@ -7,5 +10,13 @@ function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
     
 }
+function toggleMobileMenu(){
+    /*(desktopMenu.classList.contains('inactive'))
+    ?desktopMenu.classList.remove('inactive')
+    :desktopMenu.classList.add('inactive');*/
+    mobileMenu.classList.toggle('inactive');
+    
+}
 
+hamIconMenu.addEventListener('click', toggleMobileMenu)
 navMenu.addEventListener('click', toggleDesktopMenu)
