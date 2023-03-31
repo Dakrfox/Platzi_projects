67592a85fee8ffa0090d048e824b57a18f67c4ae
Dakrfox <business.nimh@outlook.com>
2023-03-31 16:01:02 -0500

    css loader

--- Fullstack Developer/Webpack/Proyecto/js-portfolio-main/webpack.config.js
+++ Fullstack Developer/Webpack/Proyecto/js-portfolio-main/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const htmlWebpackPlugin = require('html-webpack-plugin');
+const miniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -20,6 +21,12 @@ module.exports = {
                 use: {
                     loader: 'babel-loader'
                 }
+            },{
+                test: /\.css|.styl$/i ,
+                use: [miniCssExtractPlugin.loader,
+                'css-loader',
+                'stylus-loader'
+            ], 
             }
         ]
     },
@@ -28,6 +35,7 @@ module.exports = {
             infect: true,
             template: './public/index.html',
             filename: './index.html'
-        })
+        }),
+        new miniCssExtractPlugin(),
     ]
 }
