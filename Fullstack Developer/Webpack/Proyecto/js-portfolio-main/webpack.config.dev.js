c1c2d9e8208b3029a67b544f8b7dfd9c58b50ed9
Dakrfox <business.nimh@outlook.com>
2023-04-01 07:48:31 -0500

    observer watch:true

--- Fullstack Developer/Webpack/Proyecto/js-portfolio-main/webpack.config.dev.js
+++ Fullstack Developer/Webpack/Proyecto/js-portfolio-main/webpack.config.dev.js
@@ -15,6 +15,7 @@ module.exports = {
         assetModuleFilename: "assets/images/[hash][ext][query]"
     },
     mode: 'development',
+    watch:true,
     resolve: {
         extensions: ['.js'],
         alias:{
