23bea69fccd26f82a693a61a3c9f5d79f569c055
Dakrfox <business.nimh@outlook.com>
2023-03-31 20:10:58 -0500

    fonts loader

--- Fullstack Developer/Webpack/Proyecto/js-portfolio-main/webpack.config.js
+++ Fullstack Developer/Webpack/Proyecto/js-portfolio-main/webpack.config.js
@@ -8,6 +8,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.js',
+        assetModuleFilename: "assets/images/[hash][ext][query]"
     },
     resolve: {
         extensions: ['.js']
@@ -32,6 +33,18 @@ module.exports = {
             {
                 test: /\.png/, 
                 type: "asset/resource"
+            },
+            {
+                test: /\.(woff|woff2)$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 10000,
+                    mimetype: "application/font-woff",
+                    name: "[name].[ext]",
+                    outputPath: "./assets/fonts/",
+                    publicPath: "./assets/fonts/",
+                    esModule: false,
+                },
             },
         ]
     },
