0f0c4554b553fd6848de59e4bcee602cd0eefca3
Dakrfox <business.nimh@outlook.com>
2023-03-31 21:56:12 -0500

    optimization, hash

--- Fullstack Developer/Webpack/Proyecto/js-portfolio-main/webpack.config.js
+++ Fullstack Developer/Webpack/Proyecto/js-portfolio-main/webpack.config.js
@@ -2,16 +2,26 @@ const path = require('path');
 const htmlWebpackPlugin = require('html-webpack-plugin');
 const miniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
 
 module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js',
+        filename: '[name][contenthash].js',
         assetModuleFilename: "assets/images/[hash][ext][query]"
     },
     resolve: {
-        extensions: ['.js']
+        extensions: ['.js'],
+        alias:{
+            '@utils': path.resolve(__dirname, 'src/utils/'),
+            '@templates': path.resolve(__dirname, 'src/templates/'),
+            '@styles': path.resolve(__dirname, 'src/styles/'),
+            '@images': path.resolve(__dirname, 'src/assets/images/'),
+
+        }
         //react es una extension y se establece aqui
     },
     module: {
@@ -40,9 +50,9 @@ module.exports = {
                 options: {
                     limit: 10000,
                     mimetype: "application/font-woff",
-                    name: "[name].[ext]",
+                    name: "[name].[contenthash].[ext]",
                     outputPath: "./assets/fonts/",
-                    publicPath: "./assets/fonts/",
+                    publicPath: "../assets/fonts/",
                     esModule: false,
                 },
             },
@@ -54,7 +64,9 @@ module.exports = {
             template: './public/index.html',
             filename: './index.html'
         }),
-        new miniCssExtractPlugin(),
+        new miniCssExtractPlugin({
+            filename: 'assets/[name].[contenthash].css'
+        }),
         new CopyPlugin({
             patterns: [
                 {
@@ -63,5 +75,12 @@ module.exports = {
                 }
             ]
         }),
-    ]
+    ],
+    optimization: {
+        minimize:true,
+        minimizer: [
+            new CssMinimizerPlugin(),
+            new TerserPlugin(),
+        ]
+    }
 }
