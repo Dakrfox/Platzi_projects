853be2817df967c0a3fa6e6e04a5d774a7ce01b7
Dakrfox <business.nimh@outlook.com>
2023-04-07 14:53:34 -0500

    components styles containers pages

--- Fullstack Developer/Reactjs/curso-frontend-developer-practico/src/Routes/App.jsx
+++ Fullstack Developer/Reactjs/curso-frontend-developer-practico/src/Routes/App.jsx
@@ -6,17 +6,30 @@ import "../styles/global.scss";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import NotFound from "../pages/NotFound";
+import SendEmail from '../pages/SendEmail';
+import NewPassword from '../pages/NewPassword';
+import MyAccount from '../pages/MyAccount';
+import CreateAccount from '../pages/CreateAccount';
+import Checkout from '../pages/Checkout';
+import Orders from '../pages/Orders';
+
 
 const App = ()=>{
     return (
         <BrowserRouter> 
         <Layout>
                 <Switch>
-                    <Route exact path="/" component={Home }/>
-                    <Route exact path="/login" component={Login }/>
-                    <Route exact path="/password-recovery" component={PasswordRecovery}/>
-                    <Route exact path="*" component={NotFound}/>
-                </Switch>
+                <Route exact path="/" component={Home} />
+					<Route exact path="/login" component={Login} />
+					<Route exact path="/password-recovery" component={PasswordRecovery} />
+					<Route exact path="/send-email" component={SendEmail} />
+					<Route exact path="/new-password" component={NewPassword} />
+					<Route exact path="/account" component={MyAccount} />
+					<Route exact path="/signup" component={CreateAccount} />
+					<Route exact path="/checkout" component={Checkout} />
+					<Route exact path="/orders" component={Orders} />
+					<Route path="*" component={NotFound} />
+				</Switch>
             </Layout>
         </BrowserRouter>
 
