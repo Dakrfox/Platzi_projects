308ec5e49682863dadd7672c530241b87422fc0a
Dakrfox <business.nimh@outlook.com>
2023-04-08 23:26:59 -0500

    useState

--- Fullstack Developer/Reactjs/curso-frontend-developer-practico/src/components/ProductItem.jsx
+++ Fullstack Developer/Reactjs/curso-frontend-developer-practico/src/components/ProductItem.jsx
@@ -1,23 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "@styles/ProductItem.scss"
 //imagenes
 import btnAdd from "@icons/bt_add_to_cart.svg";
 
-const ProductItem = () =>{
-    return (
-        <div className="ProductItem">
+const ProductItem = () => {
+	const [cart, setCart] = useState('');
+
+	const handleClick = () => {
+		setCart('prueba');
+	}
+
+	return (
+		<div className="ProductItem">
 			<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
 			<div className="product-info">
 				<div>
 					<p>$120,00</p>
 					<p>Bike</p>
 				</div>
-				<figure>
+				<figure onClick={handleClick} >
 					<img src={btnAdd} alt="" />
 				</figure>
+				{cart}
 			</div>
 		</div>
-    );
+	);
 }
 
 export default ProductItem;
