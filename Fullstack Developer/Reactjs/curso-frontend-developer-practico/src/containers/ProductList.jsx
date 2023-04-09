c2d0013282ffd1d3ab175836e6d7388e3ac44ab6
Dakrfox <business.nimh@outlook.com>
2023-04-09 00:57:59 -0500

    custom hook

--- Fullstack Developer/Reactjs/curso-frontend-developer-practico/src/containers/ProductList.jsx
+++ Fullstack Developer/Reactjs/curso-frontend-developer-practico/src/containers/ProductList.jsx
@@ -1,20 +1,17 @@
-import React, {useState, useEffect}from 'react';
+import React from 'react';
 import ProductItem from '@components/ProductItem';
 import '@styles/ProductList.scss';
-import axios from 'axios';
+import useGetProducts from '../hooks/useGetProducts';
 
 const ProductList = () => {
-	const [products, setProducts] = useState([]);
-	useEffect(async()=>{
-		const response = await axios(API);
-		setProducts(response.data)
-	},[])
-	const API = 'https://api.escuelajs.co/api/v1/products'
+	const API = 'https://api.escuelajs.co/api/v1/products';
+	const products = useGetProducts(API);
+	
 	return (
 		<section className="main-container">
 			<div className="ProductList" >
 				{products.map(element => (
-					<ProductItem />
+					<ProductItem product = {element} key={element.id} />
 				))}
 				
 			</div>
