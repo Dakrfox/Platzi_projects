a4242b9e0644a2b673a872c886faabd4e216e758
Dakrfox <business.nimh@outlook.com>
2023-04-09 00:09:45 -0500

    useEffect API call

--- Fullstack Developer/Reactjs/curso-frontend-developer-practico/src/containers/ProductList.jsx
+++ Fullstack Developer/Reactjs/curso-frontend-developer-practico/src/containers/ProductList.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, {useState, useEffect}from 'react';
 import ProductItem from '@components/ProductItem';
 import '@styles/ProductList.scss';
+import axios from 'axios';
 
 const ProductList = () => {
+	const [products, setProducts] = useState([]);
+	useEffect(async()=>{
+		const response = await axios(API);
+		setProducts(response.data)
+	},[])
+	const API = 'https://api.escuelajs.co/api/v1/products'
 	return (
 		<section className="main-container">
-			<div className="ProductList">
-				<ProductItem />
+			<div className="ProductList" >
+				{products.map(element => (
+					<ProductItem />
+				))}
+				
 			</div>
 		</section>
 	);
