1a3e6f4fc80cd81e45d0d62f6a50e54b87a2673a
Dakrfox <business.nimh@outlook.com>
2023-04-09 20:08:57 -0500

    cart order

--- Fullstack Developer/Reactjs/curso-frontend-developer-practico/src/components/Header.jsx
+++ Fullstack Developer/Reactjs/curso-frontend-developer-practico/src/components/Header.jsx
@@ -6,12 +6,13 @@ import MenuIcon from "@icons/icon_menu.svg";
 import Logo from "../assets/logos/logo_yard_sale.svg";
 import ShoppingCart from "@icons/icon_shopping_cart.svg";
 import AppContext from '../context/AppContext';
-
+import MyOrder from "@containers/MyOrder"
 
 
 const Header = () => {
     const [toggle, setToggle] = useState(false);
     const {state} = useContext(AppContext);
+    const [toggleOrders, setToggleOrders] = useState(false);
     const handleToggle = () =>{
         setToggle(true);
     }
@@ -49,13 +50,14 @@ const Header = () => {
                 <div className="navbar-right">
                     <ul>
                         <li className="navbar-email" onClick={handleToggle}>platzi@example.com</li>
-                        <li className="navbar-shopping-cart">
+                        <li className="navbar-shopping-cart" onClick={()=>{setToggleOrders(!toggleOrders)}}>
                             <img src={ShoppingCart} alt="shopping cart"/>
                             {state.cart.length > 0 ? <div>{state.cart.length}</div>: null}
                         </li>
                     </ul>
                 </div>
                 {toggle && <MenuComponent />}
+                {toggleOrders && <MyOrder />}
         </nav>
 
     );
