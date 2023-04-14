0caa2285446a7f1622cd1b303fb90c85e6b8b553
Dakrfox <business.nimh@outlook.com>
2023-04-14 15:37:49 -0500

    next config

--- Fullstack Developer/Nextjs/platzi-shop/next.config.js
+++ Fullstack Developer/Nextjs/platzi-shop/next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    customkey: 'customValue',
+  },
+  basePath : '/dist',
+  compress: true,
+  async redirect () {
+    return [
+      {
+        source: '/hola',
+        destination: 'https://x.com',
+        permanent: true,
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
