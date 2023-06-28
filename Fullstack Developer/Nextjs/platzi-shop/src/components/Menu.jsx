76fd78557ca35a5284634c95fe32ae4038c40558
Dakrfox <business.nimh@outlook.com>
2023-06-27 20:09:37 -0500

    init backend project

--- Fullstack Developer/Nextjs/platzi-shop/src/components/Menu.jsx
+++ Fullstack Developer/Nextjs/platzi-shop/src/components/Menu.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import style from '@styles/Menu.module.scss';
 
+
 const Menu = () => {
+	
 	return (
 		<div className={style.Menu}>
 			<ul>
