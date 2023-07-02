e64db540dc94600d5edc35083b062febf70b6b3b
Dakrfox <business.nimh@outlook.com>
2023-07-02 17:51:35 -0500

    relationships

--- Fullstack Developer/BackEnd con postgreSQL/services/product.service.js
+++ Fullstack Developer/BackEnd con postgreSQL/services/product.service.js
@@ -1,14 +1,13 @@
-const { faker } = require('@faker-js/faker');
+const {faker} = require('@faker-js/faker');
 const boom = require('@hapi/boom');
-const sequelize = require('../libs/sequelize');
 
+const { models } = require('../libs/sequelize');
 
 class ProductsService {
 
   constructor(){
     this.products = [];
     this.generate();
-
   }
 
   generate() {
@@ -25,18 +24,15 @@ class ProductsService {
   }
 
   async create(data) {
-    const newProduct = {
-      id: faker.datatype.uuid(),
-      ...data
-    }
-    this.products.push(newProduct);
+    const newProduct = await models.Product.create(data);
     return newProduct;
   }
 
   async find() {
-    const query  = 'SELECT * FROM tasks';
-    const [data, metadata] = await sequelize.query(query);
-    return {data, metadata};
+    const products = await models.Product.findAll({
+      include: ['category']
+    });
+    return products;
   }
 
   async findOne(id) {
