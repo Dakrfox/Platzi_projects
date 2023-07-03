923c52bf2e1c102897372c329f521a70ebedc6b4
Dakrfox <business.nimh@outlook.com>
2023-07-02 20:33:26 -0500

    filters

--- Fullstack Developer/BackEnd con postgreSQL/schemas/product.schema.js
+++ Fullstack Developer/BackEnd con postgreSQL/schemas/product.schema.js
@@ -7,6 +7,12 @@ const description = Joi.string().min(10);
 const image = Joi.string().uri();
 const categoryId = Joi.number().integer();
 
+const price_min = Joi.number().integer();
+const price_max = Joi.number().integer();
+
+const limit = Joi.number().integer();
+const offset = Joi.number().integer();
+
 const createProductSchema = Joi.object({
   name: name.required(),
   price: price.required(),
@@ -27,4 +33,15 @@ const getProductSchema = Joi.object({
   id: id.required(),
 });
 
-module.exports = { createProductSchema, updateProductSchema, getProductSchema }
+const queryProductSchema = Joi.object({
+  limit,
+  offset,
+  price,
+  price_min,
+  price_max: price_max.when('price_min', {
+    is: Joi.number().integer(),
+    then: Joi.required()
+  })
+});
+
+module.exports = { createProductSchema, updateProductSchema, getProductSchema, queryProductSchema }
